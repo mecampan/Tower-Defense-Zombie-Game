8d1556299c2bfce0eec3be5ea19d0fc1de3b1720
mecampan <mecampan@ucsc.edu>
2024-12-03 12:55:23 -0800

    Game Start mode button implemented

---
+++ Assets/_Script/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private GameObject panel; // Parent object containing the red-boxed UI elements
+    [SerializeField] private GameObject startPlayModeButton; // The "Start Play Mode" button
+    [SerializeField] private PlacementSystem placementSystem;
+
+    public void StartPlayMode()
+    {
+        // Hide all red-boxed UI elements
+        if (panel != null)
+            panel.SetActive(false);
+
+        // Hide the "Start Play Mode" button
+        if (startPlayModeButton != null)
+            startPlayModeButton.SetActive(false);
+
+        Debug.Log("Transitioning to Play Mode. UI elements have been hidden.");
+        placementSystem.StopPlacement();
+
+        BeginPlayMode();
+    }
+
+    private void BeginPlayMode()
+    {
+        // Start gameplay (e.g., spawn enemies, start timers, etc.)
+        Debug.Log("Play Mode started! Gameplay logic goes here.");
+    }
+}
