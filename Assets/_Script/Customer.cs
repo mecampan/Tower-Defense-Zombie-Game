2cd35cf62aae6db0f14edc49815c0248af6c93a6
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-12-02 20:24:55 -0800

    working shopping AI for customers

--- Assets/_Script/Customer.cs
+++ Assets/_Script/Customer.cs
@@ -1,46 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Customer : Entity
 {
+    [SerializeField]
+    [Header("Customer Testing")]
     private List<Vector3Int> shoppingList = new List<Vector3Int>();
+    [SerializeField]
+    [Header("Customer Testing")]
     private Vector3Int exitPoint;
     private int currentTargetIndex = 0;
+    [SerializeField]
+    [Header("Customer Testing")]
+    private bool bShopping = false;
 
 
+    private void Start()
+    {
+        StartCoroutine(PerformShopping());
+    }
 
-    //public void Setup(List<Vector3Int> shelves, Vector3Int exit, float speed, float wait)
-    //{
-    //    // Generate a random shopping list from available shelves
-    //    int itemsToBuy = Random.Range(1, shelves.Count + 1); // 1 to all shelves
-    //    shoppingList = new List<Vector3Int>(shelves);
-    //    for (int i = shelves.Count - 1; i >= itemsToBuy; i--)
-    //    {
-    //        shoppingList.RemoveAt(Random.Range(0, shoppingList.Count));
-    //    }
+    public void Setup(List<Vector3Int> shelves, Vector3Int exit, float speed, float wait)
+    {
+        // Generate a random shopping list from available shelves
+        int itemsToBuy = Random.Range(1, shelves.Count + 1); // 1 to all shelves
+        shoppingList = new List<Vector3Int>(shelves);
+        for (int i = shelves.Count - 1; i >= itemsToBuy; i--)
+        {
+            shoppingList.RemoveAt(Random.Range(0, shoppingList.Count));
+        }
 
-    //    exitPoint = exit;
-    //    moveSpeed = speed;
-    //    waitTime = wait;
+        exitPoint = exit;
+        moveSpeed = speed;
+        waitTime = wait;
 
-    //    // Start moving toward the first shelf
-    //    StartCoroutine(PerformShopping());
-    //}
+        // Start moving toward the first shelf
+        StartCoroutine(PerformShopping());
+    }
 
-    //private IEnumerator PerformShopping()
-    //{
-    //    while (currentTargetIndex < shoppingList.Count)
-    //    {
-    //        Transform targetShelf = shoppingList[currentTargetIndex];
-    //        yield return MoveToTarget(targetShelf);
+    private IEnumerator PerformShopping()
+    {
+        while (currentTargetIndex < shoppingList.Count)
+        {
+            if (path == null)
+            {
+                Vector3Int targetShelf = shoppingList[currentTargetIndex];
+                if (FindPath(targetShelf))
+                {
+                    print("found new shelf: " + targetShelf.ToString());
+                }
+            }
+            else
+            {
+                int tmp = NavigatePath();
+                if (tmp == 0 || tmp == 2)
+                {
+                    Vector3Int targetShelf = shoppingList[currentTargetIndex];
+                    print("reached shelf: " + targetShelf.ToString());
+                    path = null;
+                    currentTargetIndex++;
+                }
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
 
-    //        yield return new WaitForSeconds(waitTime);
-    //        currentTargetIndex++;
-    //    }
+        while (true)
+        {
+            if (path == null)
+            {
+                FindPath(exitPoint);
+            }
+            else
+            {
+                int tmp = NavigatePath();
+                if (tmp == 0 || tmp == 2)
+                {
+                    if (pos.Equals(exitPoint))
+                    {
+                        break;
+                    }
+                }
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
 
-    //    yield return MoveToTarget(exitPoint);
-
-    //    Destroy(gameObject);
-    //}
+        Destroy(gameObject);
+    }
 }
--- Assets/_Script/Entity.cs
+++ Assets/_Script/Entity.cs
@@ -12,21 +12,21 @@ using UnityEngine.UI;
 public class Entity : MonoBehaviour
 {
     [SerializeField]
-    Vector3 pos;
+    protected Vector3 pos;
     [SerializeField]
     Vector3 posOffset;
     [SerializeField]
     Grid grid;
     [SerializeField]
     PlacementSystem placementSystem;
-    List<Vector3Int> path = null;
+    protected List<Vector3Int> path = null;
     GridData gridData = null;
     int currentIndex = 0;
     const int timer = 100;
     int CurrentTime = timer;
 
     protected float moveSpeed;
-    protected float waitTime;
+    protected float waitTime = 0.003f;
     // Start is called before the first frame update
     void Start()
     {
@@ -131,88 +131,25 @@ public class Entity : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
-    {
-        if (path == null)
-        {
-            for (int t = 0; t < 10; t++)
-            {
-                Vector3Int tmpTarget = new Vector3Int(math.clamp(RandomNumberGenerator.GetInt32(10) - 5, -5, 5), 0, math.clamp(RandomNumberGenerator.GetInt32(10) - 5, -5, 5));
-                if(FindPath(tmpTarget))
-                {
-                    break;
-                }
-            }
-        }
-        else
-        {
-            int tmp = NavigatePath();
-            if(tmp == 0 || tmp == 2)
-            {
-                path = null;
-            }
-        }
-    }
-
     //void Update()
     //{
-    //    transform.position = grid.CellToWorld(getIntPos()) + posOffset + getFloatOffset();
-    //    if (placementSystem == null)
+    //    if (path == null)
     //    {
-    //        print("placementSystem not set");
+    //        for (int t = 0; t < 10; t++)
+    //        {
+    //            Vector3Int tmpTarget = new Vector3Int(math.clamp(RandomNumberGenerator.GetInt32(10) - 5, -5, 5), 0, math.clamp(RandomNumberGenerator.GetInt32(10) - 5, -5, 5));
+    //            if(FindPath(tmpTarget))
+    //            {
+    //                break;
+    //            }
+    //        }
     //    }
     //    else
     //    {
-    //        if (gridData == null)
+    //        int tmp = NavigatePath();
+    //        if(tmp == 0 || tmp == 2)
     //        {
-    //            gridData = placementSystem.furnitureData;
-    //        }
-    //        if (placementSystem.furnitureData != null)
-    //        {
-    //            CurrentTime--;
-    //            if (path == null)
-    //            {
-    //                for (int t = 0; t < 10; t++)
-    //                {
-    //                    Vector3Int tmpTarget = new Vector3Int(math.clamp(RandomNumberGenerator.GetInt32(10) - 5, -5, 5), 0, math.clamp(RandomNumberGenerator.GetInt32(10) - 5, -5, 5));
-    //                    if (gridData.IsTileOpen(tmpTarget))
-    //                    {
-    //                        print("target: " + tmpTarget.ToString());
-    //                        path = Navagation.FindShortestPath(ref gridData, getIntPos(), tmpTarget, grid);
-    //                        if (path != null && path.Count > 0)
-    //                        {
-    //                            currentIndex = path.Count - 1;
-    //                            break;
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //            if (path != null && CurrentTime <= 0)
-    //            {
-    //                currentIndex--;
-    //                if (currentIndex >= 0)
-    //                {
-    //                    CurrentTime = timer;
-    //                    pos = path[currentIndex];
-    //                }
-    //                if (currentIndex <= 0)
-    //                {
-    //                    path = null;
-    //                }
-    //            }
-    //            else if (path != null && CurrentTime > 0)
-    //            {
-    //                if (currentIndex >= 0)
-    //                {
-    //                    if (currentIndex - 1 >= 0)
-    //                    {
-    //                        float factor = 1.0f * (1.0f * CurrentTime) / (1.0f * timer);
-    //                        Vector3Int tmpVecInt1 = path[currentIndex];
-    //                        Vector3Int tmpVecInt2 = path[currentIndex - 1];
-    //                        pos = new Vector3((factor * tmpVecInt1.x) + ((1.0f - factor) * tmpVecInt2.x), (factor * tmpVecInt1.y) + ((1.0f - factor) * tmpVecInt2.y), (factor * tmpVecInt1.z) + ((1.0f - factor) * tmpVecInt2.z));
-    //                    }
-    //                }
-    //            }
+    //            path = null;
     //        }
     //    }
     //}
